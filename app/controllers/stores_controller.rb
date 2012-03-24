5c0a3b7f3fd11f2a0b754411170116daceef0d0f
Terence Lim <weishanl@andrew.cmu.edu>
2012-03-24 01:32:01 -0400

    Stores view working

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,25 @@
+class StoresController < ApplicationController
+
+  def index
+    @stores = Store.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit  
+  end
+
+  def create
+  end
+
+  def update    
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Creamery2012::Application.routes.draw do
 
+  resources :stores
+  resources :employees
+  resources :assignments
+  resources :jobs
 end
