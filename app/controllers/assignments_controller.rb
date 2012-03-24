646f7cc7e183225e8ac12fd6833d5fe3ee3f771f
Terence Lim <weishanl@andrew.cmu.edu>
2012-03-24 01:43:24 -0400

    Generated bootstrap for assignments

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,2 +1,27 @@
 class AssignmentsController < ApplicationController
+  
+  def index
+    @assignments = Assignment.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit  
+  end
+
+  def create
+  end
+
+  def update    
+  end
+
+  def destroy
+  end
+
+
+  
 end
