5fa9239aae91d3915f0e98b7c20c6f5eab5284cc
Terence Lim <weishanl@andrew.cmu.edu>
2012-04-01 13:36:27 -0400

    All works except Add New Employees

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,49 @@
 class EmployeesController < ApplicationController
+  
+  def index
+    @employees = Employee.alphabetical.paginate(:page => params[:page]).per_page(10)
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def create
+    @employee = Employee.new(params[:employee])
+    if @employee.save
+      # if saved to database
+      flash[:notice] = "Successfully created #{@employee.name}."
+      redirect_to @employee # go to show employee page
+    else
+      # return to the 'new' form
+      render :action => 'new'
+    end
+
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    if @employee.update_attributes(params[:employee])
+      flash[:notice] = "Successfully updated #{@employee.name}."
+      redirect_to @employee
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+      @employee = Employee.find(params[:id])
+      @employee.destroy
+      flash[:notice] = "Successfully removed #{@employee.name} from the Creamery system."
+      redirect_to employees_url
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,22 @@ Creamery2012::Application.routes.draw do
   resources :employees
   resources :assignments
   resources :jobs
+  resources :shifts
+  
+  # Authentication routes
+  match 'user/edit' => 'users#edit', :as => :edit_current_user
+  match 'signup' => 'users#new', :as => :signup
+  match 'logout' => 'sessions#destroy', :as => :logout
+  match 'login' => 'sessions#new', :as => :login
+
+  # Semi-static page routes
+  match 'home' => 'home#home', :as => :home
+  match 'about' => 'home#about', :as => :about
+  match 'contact' => 'home#contact', :as => :contact
+  match 'privacy' => 'home#privacy', :as => :privacy
+  match 'search' => 'home#search', :as => :search
+  
+  # Set the root url
+  root :to => 'home#home'
+  
 end
