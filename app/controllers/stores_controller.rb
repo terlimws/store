6217ad71e78f21b129c9509e2b33728a2f82e66b
Terence Lim <weishanl@andrew.cmu.edu>
2012-04-08 13:44:16 -0400

    Just before cucumber tests

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -4,7 +4,7 @@ class StoresController < ApplicationController
   authorize_resource
 
   def index
-    @stores = Store.alphabetical.paginate(:page => params[:page]).per_page(10)
+    @stores = Store.active.alphabetical.paginate(:page => params[:page]).per_page(10)
   end
 
   def show
